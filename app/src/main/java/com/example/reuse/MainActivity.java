a88cd6df13080cdec465687b909174f0c91701cc
MD Musaddique Ali Erfan <musaddique.cse.20200204049@aust.edu>
2023-01-13 20:48:46 +0600

    Register Button updated

--- app/src/main/java/com/example/reuse/MainActivity.java
+++ app/src/main/java/com/example/reuse/MainActivity.java
@@ -10,18 +10,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Handler handler = new Handler();
-    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.ReUse);
-        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/Poppins-Regular.ttf");
-        textView.setTypeface(custom_font);
         handler.postDelayed(new Runnable() {
             public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginRegister.class);
+                Intent intent = new Intent(MainActivity.this, GetStarted.class);
                 startActivity(intent);
+                finish();
             }
         },3000);
     }
