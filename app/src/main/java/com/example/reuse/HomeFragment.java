2d00d9c8a3cf6a636c76399fdc3398c1beb8e739
Tanvir Raiyan <tanvirraiyan456@gmail.com>
2023-02-06 05:47:54 +0600

    changed recyler view layout to grid layout

--- app/src/main/java/com/example/reuse/HomeFragment.java
+++ app/src/main/java/com/example/reuse/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -35,7 +36,7 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         recyclerView = view.findViewById(R.id.recyclerView);
         databaseReference = FirebaseDatabase.getInstance("https://reuse-20200204-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("UnfilteredPosts");
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),2));
 
         unfilteredPosts = new ArrayList<>();
         feedAdapter = new FeedAdapter(getContext(),unfilteredPosts);
