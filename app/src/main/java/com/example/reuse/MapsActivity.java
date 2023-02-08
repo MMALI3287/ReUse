3c4cfcfe4774d3afc93324a9ae244491df13781f
MD Musaddique Ali Erfan <musaddique.cse.20200204049@aust.edu>
2023-02-08 16:22:49 +0600

    Map LatLng added

--- app/src/main/java/com/example/reuse/MapsActivity.java
+++ app/src/main/java/com/example/reuse/MapsActivity.java
@@ -1,51 +1,180 @@
 package com.example.reuse;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.PermissionRequest;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.reuse.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+import java.io.IOException;
+import java.util.List;
 
-    private GoogleMap mMap;
-    private ActivityMapsBinding binding;
+
+public class MapsActivity extends AppCompatActivity {
+    FusedLocationProviderClient fusedLocationClient;
+    SupportMapFragment mapFragment;
+    Marker marker;
+    PlacesClient placesClient;
+    GoogleMap googleMap;
+    private EditText searchBar;
+    private Button searchBtn, returnBtn;
+    private TextView lat, lng,placeName;
+
+    private LatLng selectedLocation;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        searchBar = findViewById(R.id.search_bar);
+        searchBtn = findViewById(R.id.search_button);
+        returnBtn = findViewById(R.id.return_button);
+        lat = findViewById(R.id.latitude);
+        lng = findViewById(R.id.longitude);
+        placeName = findViewById(R.id.place_name);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        /*searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = searchBar.getText().toString();
+                if (!location.equals("")) {
+                    Geocoder geocoder = new Geocoder(MapsActivity.this);
+                    try {
+                        List<Address> addressList = geocoder.getFromLocationName(location, 1);
+                        if (addressList.size() > 0) {
+                            Address address = addressList.get(0);
+                            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                            googleMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18));
+                            selectedLocation = latLng;
+                        } else {
+                            Toast.makeText(MapsActivity.this, "Location not found", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });*/
+
+        returnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("latitude", selectedLocation.latitude);
+                resultIntent.putExtra("longitude", selectedLocation.longitude);
+                resultIntent.putExtra("name", googleMap.getCameraPosition().target.toString());
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+
+        Dexter.withContext(getApplicationContext())
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        getMyLocation();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        if (response.isPermanentlyDenied()) {
+                            //open settings
+
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(com.karumi.dexter.listener.PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
     }
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16f));
+
+    private void getMyLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = fusedLocationClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                mapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        if (location != null) {
+                            selectedLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            MarkerOptions markerOptions = new MarkerOptions().position(selectedLocation).title("Your current Location");
+                            marker=googleMap.addMarker(markerOptions);
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(selectedLocation, 18));
+                            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                                @Override
+                                public void onMapClick(LatLng point) {
+                                    double latitude = point.latitude;
+                                    double longitude = point.longitude;
+                                    System.out.println(latitude + " " + longitude);
+                                    lat.setText("Latitude:\n"+latitude);
+                                    lng.setText("Longitude:\n"+longitude);
+                                    marker.setPosition(new LatLng(latitude, longitude));
+
+                                    //placeName.setText("Place name: "+googleMap.getCameraPosition().target.toString());
+
+                                }
+                            });
+                        }
+                        else if (location == null) {
+                            System.out.println("Location is null");
+                        }
+                        else{
+                            System.out.println("Something went wrong");
+                        }
+                    }
+                });
+            }
+        });
     }
-    @Override
-    public void onMapClick(LatLng point) {
-        System.out.println("Ekhane");
-        double latitude = point.latitude;
-        double longitude = point.longitude;
-        Log.d("Ekhane Latitude", String.valueOf(latitude));
-        Log.d("Ekhane Longitude", String.valueOf(longitude));
-    }
+
 }
